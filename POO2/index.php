ffed8cb94e8e5ae581dc8dc1bc1c477e4711fcf0
Jaaziel Silva <143227938+JaazielSlv@users.noreply.github.com>
2025-04-28 00:51:13 -0300

    Add files via upload

---
+++ POO2/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once "eletronicos.php";
+require_once "tv.php";
+
+// $produto = new produto();
+
+// $produto->setdescricao("teclado");
+// $produto->setpreco(100);
+
+// $produto->apresentar();
+
+
+$tv = new tv ("smart tv", 43, "LG");
+
+$tv->apresentar();
+
+
+
+
+?>
